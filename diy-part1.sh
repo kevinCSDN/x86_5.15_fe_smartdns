4d36263409227a30deacba1ceabb22dbf673fb11
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-03-08 22:01:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,21 +11,12 @@
 #
 
 # Uncomment a feed source
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#删除feeds自带mosdns、v2ray-geodata
-rm -rf ./feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/luci/themes/luci-theme-argon
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
-
-
-
-
-
-
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 
 #克隆passwall环境插件
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 
 #克隆的源码放在small文件夹,预先建立small文件夹
 mkdir package/small
@@ -35,12 +26,10 @@ pushd package/small
 
 #passwall2
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+#git clone -b luci-smartdns-dev --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 #mosdns
-# git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
-git clone --depth=1 https://github.com/fw876/helloworld.git
-# git clone https://github.com/kevinCSDN/lua-maxminddb.git  #git lua-maxminddb 依赖
-
-# git clone https://github.com/kevinCSDN/luci-app-vssr-plus.git  
+#git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+#git clone --depth=1 https://github.com/fw876/helloworld.git
 
 popd
